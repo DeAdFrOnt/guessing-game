7d01bb5e859da01a4eb1634075953f1784bab2ef
DeAdFrOnt <semenukmaks@gmail.com>
2019-10-14 04:41:44 +0300

    feat:solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,17 @@
 class GuessingGame {
     constructor() {}
-
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        return this.middle = Math.round((this.min + this.max) / 2);
     }
-
     lower() {
-
+        this.max = this.middle;
     }
-
     greater() {
-
+        this.min = this.middle;
     }
 }
-
 module.exports = GuessingGame;
